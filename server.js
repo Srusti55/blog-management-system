17a67eaca0515691f61e19b7cfac59153a413bb9
Srusti55 <nagursrusti@gmail.com>
2026-07-22 21:11:30 +0530

    Day 5 - Created Express server with GET and POST routes

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Blog Management System");
 });
+
+
+app.post("/add-blog", (req, res) => {
+
+    const blog = req.body;
+
+    res.send({
+        message: "Blog Added Successfully",
+        data: blog
+    });
+
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
